d7e886dffa9bd15ad2aac296454175b85cdc88db
Reginaldo Silva <reginaldolimas@gmail.com>
2023-09-04 14:47:56 -0300

    Fix: card style

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { Card, Button } from 'antd';
+import { Card, Button, Space } from 'antd';
 const { Meta } = Card;
-export const CardAnd = ({ id, image, title, total, onClick}) => (
-  <Card
-    hoverable
-    style={{
-      width: 240,
-    }}
-    cover={<img alt="example" src={image} />}
-  >
-    <Meta title={title} description="" />
-  </Card>
+export const CardAnd = ({ id, image, title, total, onClick }) => (
+    <Card
+        hoverable
+        style={{
+            width: 240,
+        }}
+        cover={<img alt="example" src={image} />}
+    >
+        <Space align="center" style={{display: "flex", justifyContent: "space-between"}}>
+            <Meta title={title} description="" />
+            <Button type="primary">Salvar</Button>
+        </Space>
+
+    </Card>
 );
